16b6673cea2c42cb043c4cfb53ae3be4c745d140
Jigar Vasani <jigarvasani@Jigars-MacBook-Pro.local>
2018-08-09 00:45:17 -0400

    Add Portfolio tiles on portfolio.js

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import LandingPage from "@pages/LandingPage";
+import AboutPage from "@pages/AboutPage";
+import PortfolioPage from "@pages/PortfolioPage";
+import ParallaxBackground from "@components/ParallaxBackground";
+
+import ScrollTop from "@components/ScrollTop";
+import ThemeSwitcher from "@components/ThemeSwitcher";
+import Browser from "@components/Browser";
+import Footer from "@components/Footer";
+import ContactMePage from "@pages/ContactMePage";
+
+class App extends Component {
+  render() {
+    return (
+     
+      <div className="dev-landing-page" id="root">
+        <ThemeSwitcher>
+          <Browser except firefox>
+            <ParallaxBackground />
+          </Browser>
+          <LandingPage />
+          <AboutPage />
+          <PortfolioPage />
+          <ContactMePage />
+          <ScrollTop />
+          <Footer />
+        </ThemeSwitcher>
+      </div>
+    );
+  }
+
+  
+}
+
+export default App;
---
+++ src/pages/PortfolioPage/portfolio-items.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+
+export default [{
+
+  name: 'YouCRM',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        YouCRM
+      </div>
+      <div className='portfolio-item__desc'>
+        CRM is an approach to manage a company's interaction with current and potential customers. 
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-js"></i>
+        <i className="fab fa-java"></i>
+        <i className="fab fa-html5"></i>
+        <i className="fab fa-css3"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        {/* <a target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/package/typy">NPM</a> */}
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jvasani/YouCRM">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'AES 1.0',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        AES 1.0
+      </div>
+      <div className='portfolio-item__desc'>
+        AES 128 and 256 bit encryption and decryption
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-java"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jvasani/AES-1.0">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'JigarV Portfolio Website',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        JigarV Portfolio Website
+      </div>
+      <div className='portfolio-item__desc'>
+       Personal portfolio website
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-react"></i>
+        <i className="fab fa-js"></i>
+        <i className="fab fa-css3"></i>
+        <i className="fab fa-npm"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jvasani/JigarV">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'npm-module-boilerplate',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        NPM Module Boilerplate
+      </div>
+      <div className='portfolio-item__desc'>
+         Boilerplate plate code to create and publish an NPM package
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-js"></i>
+        <i className="fab fa-npm"></i>
+      </div>
+      <div className='portfolio-item__links'>
+       
+        {/* <a target="_blank" rel="noopener noreferrer" href="https://github.com/flexdinesh/npm-module-boilerplate">More</a> */}
+      </div>
+    </div>
+  )
+}, {
+  name: 'Weather-App',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        Weather-App
+      </div>
+      <div className='portfolio-item__desc'>
+        REST API in react which gives a detailed information on current weather forecast by city and country for now.
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-js"></i>
+       <i className = "fab fa-react"></i>
+        <i className="fab fa-npm"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jvasani/weather-app">More</a>
+      </div>
+    </div>
+  )
+}, ]
